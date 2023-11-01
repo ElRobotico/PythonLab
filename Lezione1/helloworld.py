ff7d944aa6d45477856a571d3fc85cd474231b1d
Marco <reg@marco.wf>
2023-11-01 19:29:48 +0100

    Lezione 1

---
+++ Lezione1/helloworld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+    print()
+    name = input('Come ti chiami?\n')
+    if name.lower() == 'marco':
+        print('Ciao Marco')
+    else:
+        print('Non sei il benvenuto')
+
+
+# Execute
+if __name__ == '__main__':
+    main()
